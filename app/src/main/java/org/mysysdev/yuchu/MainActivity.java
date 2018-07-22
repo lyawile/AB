3a6b797db6657501fab6b8e8d206cf043c83e829
lyawile <webdev271@gmail.com>
2018-07-22 06:31:08 +0300

    Images added, the MainActivity xml and java files edited

--- app/src/main/java/org/mysysdev/yuchu/MainActivity.java
+++ app/src/main/java/org/mysysdev/yuchu/MainActivity.java
@@ -1,7 +1,13 @@
 package org.mysysdev.yuchu;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ImageView myImage = findViewById(R.id.imageToShow);
+        final int[] imagesList = new int[]{
+                R.drawable.rose_flower,
+                R.drawable.car,
+                R.drawable.tree
+        };
+        Button btnNext =  findViewById(R.id.btnNext);
+                btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int currentImage =imagesList[2];
+                myImage.setImageResource(currentImage);
+                System.out.println(R.drawable.rose_flower);
+                System.out.println(Arrays.toString(imagesList));
+            }
+        });
+
     }
 }
